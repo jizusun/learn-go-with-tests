43ebbd1f8a9cf6386d879ac4d5717b428c46f680
Jizu Sun <jizu.sun@sap.com>
2020-11-16 15:17:05 +0800

    spy the method call

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,10 +1,27 @@
 package mocking
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"time"
 )
 
-func Countdown(out *bytes.Buffer) {
-	fmt.Fprintf(out, "3")
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprintf(out, finalWord)
 }
